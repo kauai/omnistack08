ad6f30a37d4b631e67f5efcb5a2e67342632a8fe
Thiago kauai <html5ephp@gmail.com>
2019-08-06 12:07:17 -0300

    criando Schema mongoose

--- src/server.js
+++ src/server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const server = express()
+const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
 
 server.use(express.json())
 
 server.use(require('./routes'))
+mongoose.connect('mongodb+srv://omnistack:omnistack2012@meudinheiro-jmzoi.mongodb.net/omnistack8?retryWrites=true&w=majority',{
+     useNewUrlParser:true
+})
 
 server.listen(port,e => {
     if(e) console.log(e)
